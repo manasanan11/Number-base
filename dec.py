c107095271bc358be7e75a60d93f1ce01a3eeb59
manasanan11 <tawyukikung@gmail.com>
2026-07-06 19:48:26 +0700

    Add files via upload

---
+++ dec.py
@@ -0,0 +1,15 @@
+dec = 1234 #เลขฐาน10
+conv_to = bin(dec) #แปลงเลขฐาน10 to 2
+dec = (conv_to)
+print(dec)
+
+dec2 = 1234 #เลขฐาน10
+conv_to2 = oct(dec2) #แปลงเลขฐาน10 to 8
+dec2 = (conv_to2)
+print(dec2)
+
+dec3 = 1234 #เลขฐาน10
+conv_to3 = hex(dec3) #แปลงเลขฐาน10 to 16
+dec3 = (conv_to3)
+print(dec3)
+print('มนัศนันท์ ชินอักษร')
